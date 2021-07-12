beffd0c023ce46e47589f17a038bfa4e5a1db244
majdeddine <fazaamajdfazaa@gmail.com>
2021-07-13 01:20:56 +0200

    add coffeescript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery"
 import "stylesheets/application"
+import "packs/hello_coffee"
 
 Rails.start()
 Turbolinks.start()
